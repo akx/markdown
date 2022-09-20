c45b0a7ce283a4d3c5d13c0796f1da6c12f2d568
Aarni Koskela <akx@iki.fi>
2022-09-20 13:28:33 +0300

    DRY remark-math.spec.ts

--- __tests__/remark-math.spec.ts
+++ __tests__/remark-math.spec.ts
@@ -11,10 +11,19 @@ function remark() {
     .use(stringify);
 }
 
+function parseAST(targetText: string) {
+  const processor = remark().use(math);
+  const ast = processor.parse(targetText);
+  return ast;
+}
+
+function parseToString(targetText: string): string {
+  const processor = remark().use(math);
+  return processor.processSync(targetText).toString();
+}
+
 describe("remark-math", () => {
   it("must parse a math inline and a math block ", () => {
-    const processor = remark().use(math);
-
     const targetText = [
       "Math $\\alpha$",
       "",
@@ -22,8 +31,7 @@ describe("remark-math", () => {
       "\\beta+\\gamma",
       "$$",
     ].join("\n");
-
-    const ast = processor.parse(targetText);
+    const ast = parseAST(targetText);
 
     expect(ast).toMatchObject(
       u("root", [
@@ -34,33 +42,26 @@ describe("remark-math", () => {
   });
 
   it("must escape a dollar with backslash", () => {
-    const processor = remark().use(math);
-
     const targetText = "$\\alpha\\$";
+    const ast = parseAST(targetText);
 
-    const ast = processor.parse(targetText);
     expect(ast).toMatchObject(
       u("root", [u("paragraph", [u("text", "$\\alpha$")])])
     );
   });
 
   it("must escape all dollars with backslashes", () => {
-    const processor = remark().use(math);
-
     const targetText = "\\$\\alpha\\$";
+    const ast = parseAST(targetText);
 
-    const ast = processor.parse(targetText);
     expect(ast).toMatchObject(
       u("root", [u("paragraph", [u("text", "$"), u("text", "\\alpha$")])])
     );
   });
 
   it("must NOT escape a dollar with double backslashes", () => {
-    const processor = remark().use(math);
-
     const targetText = "\\\\$\\alpha$";
-
-    const ast = processor.parse(targetText);
+    const ast = parseAST(targetText);
 
     expect(ast).toMatchObject(
       u("root", [u("paragraph", [u("text", "\\"), u("inlineMath", "\\alpha")])])
@@ -68,22 +69,18 @@ describe("remark-math", () => {
   });
 
   it("must not parse a raw starting dollar", () => {
-    const processor = remark().use(math);
-
     const targetText = "`$`\\alpha$";
+    const ast = parseAST(targetText);
 
-    const ast = processor.parse(targetText);
     expect(ast).toMatchObject(
       u("root", [u("paragraph", [u("inlineCode", "$"), u("text", "\\alpha$")])])
     );
   });
 
   it("fooo must not parse a raw ending dollar", () => {
-    const processor = remark().use(math);
-
     const targetText = "$\\alpha`$` foo";
+    const ast = parseAST(targetText);
 
-    const ast = processor.parse(targetText);
     expect(ast).toMatchObject(
       u("root", [
         u("paragraph", [
@@ -96,22 +93,17 @@ describe("remark-math", () => {
   });
 
   it("fooo must not parse allow inline to contain backticks", () => {
-    const processor = remark().use(math);
-
     const targetText = "$`\\alpha`$";
+    const ast = parseAST(targetText);
 
-    const ast = processor.parse(targetText);
     expect(ast).toMatchObject(
       u("root", [u("paragraph", [u("inlineMath", "`\\alpha`")])])
     );
   });
 
   it("must render a super factorial to a math block", () => {
-    const processor = remark().use(math);
-
     const targetText = "$\\alpha\\$$";
-
-    const ast = processor.parse(targetText);
+    const ast = parseAST(targetText);
 
     expect(ast).toMatchObject(
       u("root", [u("paragraph", [u("inlineMath", "\\alpha\\$")])])
@@ -119,21 +111,15 @@ describe("remark-math", () => {
   });
 
   it("must render super factorial to a math inline", () => {
-    const processor = remark().use(math);
-
     const targetText = ["$$", "\\alpha\\$", "$$"].join("\n");
-
-    const ast = processor.parse(targetText);
+    const ast = parseAST(targetText);
 
     expect(ast).toMatchObject(u("root", [u("math", "\\alpha\\$")]));
   });
 
   it("must render a math block just after a pragraph", () => {
-    const processor = remark().use(math);
-
     const targetText = ["tango", "$$", "\\alpha", "$$"].join("\n");
-
-    const ast = processor.parse(targetText);
+    const ast = parseAST(targetText);
 
     expect(ast).toMatchObject(
       u("root", [u("paragraph", [u("text", "tango")]), u("math", "\\alpha")])
@@ -141,11 +127,8 @@ describe("remark-math", () => {
   });
 
   it("must parse inline math between double dollars", () => {
-    const processor = remark().use(math);
-
     const targetText = "$$\\alpha$$";
-
-    const ast = processor.parse(targetText);
+    const ast = parseAST(targetText);
 
     expect(ast).toMatchObject(
       u("root", [u("paragraph", [u("math", "\\alpha")])])
@@ -153,11 +136,8 @@ describe("remark-math", () => {
   });
 
   it("must stringify", () => {
-    const processor = remark().use(math);
-
     const targetText = ["$$\\alpha$$", "$$", "\\alpha\\beta", "$$"].join("\n");
-
-    const result = processor.processSync(targetText).toString();
+    const result = parseToString(targetText);
 
     expect(result).toEqual(
       ["$$\n\\alpha\n$$", "", "$$", "\\alpha\\beta", "$$", ""].join("\n")
@@ -165,43 +145,31 @@ describe("remark-math", () => {
   });
 
   it("must stringify math block child of blockquote", () => {
-    const processor = remark().use(math);
-
     const targetText = ["> $$", "> \\alpha\\beta", "> $$"].join("\n");
-
-    const result = processor.processSync(targetText).toString();
+    const result = parseToString(targetText)
 
     expect(result).toEqual(["> $$", "> \\alpha\\beta", "> $$", ""].join("\n"));
   });
 
   it("must parse math block with indent", () => {
-    const processor = remark().use(math);
-
     const targetText = ["  $$$", "    \\alpha", "  $$$"].join("\n");
-
-    const ast = processor.parse(targetText);
+    const ast = parseAST(targetText);
 
     expect(ast).toMatchObject(u("root", [u("math", "  \\alpha")]));
   });
 
   it("must ignore everything just after opening/closing marker", () => {
-    const processor = remark().use(math);
-
     const targetText = ["$$  must", "\\alpha", "$$  be ignored", ""].join("\n");
-
-    const ast = processor.parse(targetText);
+    const ast = parseAST(targetText);
 
     expect(ast).toMatchObject(u("root", [u("math", "\\alpha")]));
   });
 
   it("must not affect next block", () => {
-    const processor = remark().use(math);
-
     const targetText = ["$$", "\\alpha", "$$", "```", "code fence", "```"].join(
       "\n"
     );
-
-    const ast = processor.parse(targetText);
+    const ast = parseAST(targetText);
 
     expect(ast).toMatchObject(
       u("root", [u("math", "\\alpha"), u("code", "code fence")])
@@ -209,11 +177,8 @@ describe("remark-math", () => {
   });
 
   it("must not set inlineMathDouble class", () => {
-    const processor = remark().use(math);
-
     const targetText = "$$\\alpha$$";
-
-    const ast = processor.parse(targetText);
+    const ast = parseAST(targetText);
 
     expect(ast).toEqual(
       u("root", [
@@ -236,45 +201,9 @@ describe("remark-math", () => {
     );
   });
 
-  // it("must set inlineMathDouble class if inlineMathDouble is true", () => {
-  //   const processor = remark().use(math, {
-  //     inlineMathDouble: true
-  //   });
-
-  //   const targetText = "hello $$\\alpha$$ world";
-
-  //   const ast = processor.parse(targetText);
-
-  //   expect(ast).toEqual(
-  //     u("root", [
-  //       u("paragraph", [
-  //         u("text", "hello "),
-  //         u(
-  //           "math",
-  //           {
-  //             data: {
-  //               hChildren: [u("text", "\\alpha")],
-  //               hName: "div",
-  //               hProperties: {
-  //                 className: "math inlineMathDouble"
-  //               }
-  //             }
-  //           },
-  //           "\\alpha"
-  //         ),
-  //         u("text", " world")
-  //       ])
-  //     ])
-  //   );
-  // });
-
   it("must parse more complex math equations in math block", () => {
-    const processor = remark().use(math);
-
-    const targetText =
-      "$$p(\\theta_i \\thinspace | \\, \\{\\theta_{j \\neq i}\\}, D)$$";
-
-    const ast = processor.parse(targetText);
+    const targetText = "$$p(\\theta_i \\thinspace | \\, \\{\\theta_{j \\neq i}\\}, D)$$";
+    const ast = parseAST(targetText);
 
     expect(ast).toEqual(
       u("root", [
@@ -303,12 +232,8 @@ describe("remark-math", () => {
   });
 
   it("must parse more complex math equations inline math", () => {
-    const processor = remark().use(math);
-
-    const targetText =
-      "$p(\\theta_i \\thinspace | \\, \\{\\theta_{j \\neq i}\\}, D)$";
-
-    const ast = processor.parse(targetText);
+    const targetText = "$p(\\theta_i \\thinspace | \\, \\{\\theta_{j \\neq i}\\}, D)$";
+    const ast = parseAST(targetText);
 
     expect(ast).toEqual(
       u("root", [
